330c19d89cb3347472055463b1350341c70f8647
Bailey Cage <bailey.cage@gmail.com>
2022-08-10 09:43:34 -0400

    add func fuzzyMatch()

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function findMatching (driverAry, name) {
     return driverAry.filter(element => element.toLowerCase() === name.toLowerCase());
 }
+
+function fuzzyMatch (driverAry, begLtrs) {
+    const numLtrs = begLtrs.length;
+    return driverAry.filter(element => element.slice(0, numLtrs) === begLtrs);
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+fuzzyMatch(drivers, "Sa");
