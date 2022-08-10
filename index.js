81baf179b513ea5476667736f45786c73fa3e6fa
Bailey Cage <bailey.cage@gmail.com>
2022-08-10 09:45:10 -0400

    add func matchName()

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ function fuzzyMatch (driverAry, begLtrs) {
     return driverAry.filter(element => element.slice(0, numLtrs) === begLtrs);
 }
 
+function matchName (driverAry, name) {
+    return driverAry.filter(element => element.name === name);
+}
+
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
 fuzzyMatch(drivers, "Sa");
